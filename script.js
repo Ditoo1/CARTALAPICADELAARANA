a4c216585860d2038c7553e6cd92386558c55f53
Ditoo1 <revecod.ignacio@gmail.com>
2024-01-11 03:31:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     var imageList = document.getElementById("imageList");
     var images = imageList.getElementsByTagName("img");
-    var imageHeight = images[0].offsetHeight;
 
-    // Establece la posición inicial de desplazamiento al comienzo
-    imageList.scrollTop = 0;
+    // Calcula la altura total de las imágenes en el contenedor
+    var totalImageHeight = Array.from(images).reduce(function (total, img) {
+        return total + img.offsetHeight;
+    }, 0);
+
+    // Calcula la posición inicial de desplazamiento para centrar la primera imagen
+    imageList.scrollTop = (totalImageHeight - window.innerHeight) / 2;
 
     imageList.addEventListener("scroll", function () {
         var scrollTop = imageList.scrollTop;
 
         // Calcula el índice de la imagen visible
-        var visibleImageIndex = Math.floor(scrollTop / imageHeight);
+        var visibleImageIndex = Math.floor(scrollTop / window.innerHeight);
 
         for (var i = 0; i < images.length; i++) {
             var image = images[i];
-            var distanceFromTop = image.offsetTop - scrollTop;
             var isCurrentImage = i === visibleImageIndex;
 
             // Aplica estilos solo a la imagen visible
             if (isCurrentImage) {
-                var scaleFactor = Math.max(0, 1 - Math.abs(distanceFromTop) / imageList.clientHeight);
+                var distanceFromTop = image.offsetTop - scrollTop;
+                var scaleFactor = Math.max(0, 1 - Math.abs(distanceFromTop) / window.innerHeight);
                 image.style.transform = "scale(" + scaleFactor + ")";
                 image.style.zIndex = scaleFactor === 0 ? -1 : 1;
                 image.style.filter = "blur(" + (1 - scaleFactor) * 10 + "px)";
