f0b5c3212b2a6026d20965b33f826db61b0bc969
Ditoo1 <revecod.ignacio@gmail.com>
2024-01-11 03:27:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
         for (var i = 0; i < images.length; i++) {
             var image = images[i];
             var distanceFromTop = image.offsetTop - scrollTop;
-            var scaleFactor = 1 - Math.abs(distanceFromTop) / imageList.clientHeight;
+            var scaleFactor = Math.max(0, 1 - Math.abs(distanceFromTop) / imageList.clientHeight);
 
             // Aplica estilos basados en la posición de desplazamiento
             image.style.transform = "scale(" + scaleFactor + ")";
-            image.style.zIndex = scaleFactor * 1000;
+            image.style.zIndex = scaleFactor === 0 ? -1 : 1;
             image.style.filter = "blur(" + (1 - scaleFactor) * 10 + "px)";
         }
     });
